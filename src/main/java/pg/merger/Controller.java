0253b52632c09076ea7616ee8ac0c2a204a878b6
PreCyz <precpaw@op.pl>
2020-04-09 22:59:49 +0200

    Initial commit

---
+++ src/main/java/pg/merger/Controller.java
@@ -0,0 +1,91 @@
+package pg.merger;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+import org.apache.pdfbox.io.MemoryUsageSetting;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+public class Controller implements Initializable {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    @FXML
+    private Button choosePdfsButton;
+    @FXML
+    private Button mergePdfsButton;
+    @FXML
+    private TextArea infoTextArea;
+
+    private LinkedList<File> files;
+    private String destinationDir;
+
+    private final Window parent;
+
+    public Controller(Window parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        choosePdfsButton.setOnAction(choosePdfsAction());
+        mergePdfsButton.setOnAction(mergePdfsAction());
+    }
+
+    private EventHandler<ActionEvent> choosePdfsAction() {
+        return actionEvent -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setInitialDirectory(new File("."));
+            fileChooser.setTitle("Choose PDFs");
+            files = new LinkedList<>(
+                    Optional.ofNullable(fileChooser.showOpenMultipleDialog(parent)).orElseGet(Collections::emptyList)
+            );
+            if (!files.isEmpty()) {
+                infoTextArea.setText(files.stream().map(File::getName).collect(Collectors.joining("\n")));
+                destinationDir = files.getFirst().toPath().getParent().toString();
+            }
+        };
+    }
+
+    private EventHandler<ActionEvent> mergePdfsAction() {
+        return actionEvent -> {
+            if (files.isEmpty()) {
+                infoTextArea.setText("Nothing to merge");
+            } else {
+                mergePdfs();
+            }
+        };
+    }
+
+    private void mergePdfs() {
+        String destinationFileName = String.format("%s%sresult.pdf", destinationDir, File.separator);
+        try {
+            PDFMergerUtility ut = new PDFMergerUtility();
+            for (File file : files) {
+                ut.addSource(file);
+            }
+            ut.setDestinationFileName(destinationFileName);
+            ut.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
+            infoTextArea.setText("Files merged. Find the result.pdf in here");
+        } catch (IOException e) {
+            logger.error("Something went wrong.", e);
+            infoTextArea.setText("Something went wrong :(");
+        }
+    }
+}
---
+++ src/main/java/pg/merger/Main.java
@@ -0,0 +1,44 @@
+package pg.merger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main extends Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            primaryStage.setOnCloseRequest(windowEvent -> System.exit(0));
+            primaryStage.setTitle("PdfMerger 1.0");
+            primaryStage.show();
+            primaryStage.setResizable(false);
+            Scene scene = new Scene(root(primaryStage));
+            primaryStage.setScene(scene);
+            primaryStage.sizeToScene();
+        } catch (IOException ex) {
+            logger.error("Building scene error.", ex);
+        }
+    }
+
+    private Parent root(Window parent) throws IOException {
+        URL url = Main.class.getClassLoader().getResource(String.format("%s/%s", "fxml", "main.fxml"));
+        FXMLLoader loader = new FXMLLoader(url);
+        loader.setController(new Controller(parent));
+        return loader.load();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

1f602f7e298c6859af443f7ade58e7a695f75f18
Precyzik <gawaprecyz@gmail.com>
2023-09-30 13:03:23 +0200

    Add sorts.

--- src/main/java/pg/merger/Controller.java
+++ src/main/java/pg/merger/Controller.java
@@ -1,10 +1,12 @@
 package pg.merger;
 
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
@@ -16,9 +18,15 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.ResourceBundle;
 import java.util.stream.Collectors;
 
 public class Controller implements Initializable {
@@ -31,6 +39,12 @@ public class Controller implements Initializable {
     private Button mergePdfsButton;
     @FXML
     private TextArea infoTextArea;
+    @FXML
+    private CheckBox sortByCreationDateAscCheckBox;
+    @FXML
+    private CheckBox sortByCreationDateDescCheckBox;
+    @FXML
+    private CheckBox sortByNameCheckBox;
 
     private LinkedList<File> files;
     private String destinationDir;
@@ -45,6 +59,9 @@ public class Controller implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         choosePdfsButton.setOnAction(choosePdfsAction());
         mergePdfsButton.setOnAction(mergePdfsAction());
+        sortByCreationDateDescCheckBox.selectedProperty().addListener(sortByCreationTimeChangeListener(true));
+        sortByCreationDateAscCheckBox.selectedProperty().addListener(sortByCreationTimeChangeListener(false));
+        sortByNameCheckBox.selectedProperty().addListener(sortByNameChangeListener());
     }
 
     private EventHandler<ActionEvent> choosePdfsAction() {
@@ -92,5 +109,46 @@ public class Controller implements Initializable {
             logger.error("Something went wrong.", e);
             infoTextArea.setText("Something went wrong :(");
         }
+    }
+
+    private ChangeListener<Boolean> sortByCreationTimeChangeListener(boolean isDesc) {
+        return (observableValue, oldValue, newValue) -> {
+            if (newValue) {
+                files = files.stream().sorted(creationTimeComparator(isDesc)).collect(Collectors.toCollection(LinkedList::new));
+                infoTextArea.clear();
+                infoTextArea.setText(files.stream().map(File::getName).collect(Collectors.joining("\n")));
+                sortByNameCheckBox.setSelected(false);
+                if (isDesc) {
+                    sortByCreationDateAscCheckBox.setSelected(false);
+                } else {
+                    sortByCreationDateDescCheckBox.setSelected(false);
+                }
+            }
+        };
+    }
+
+    private Comparator<File> creationTimeComparator(boolean isDesc) {
+        return (o1, o2) -> {
+            try {
+                int multipler = isDesc ? 1 : -1;
+                BasicFileAttributes f1 = Files.readAttributes(o1.toPath(), BasicFileAttributes.class);
+                BasicFileAttributes f2 = Files.readAttributes(o2.toPath(), BasicFileAttributes.class);
+                return multipler * f1.creationTime().compareTo(f2.creationTime());
+            } catch (IOException e) {
+                return 0;
+            }
+        };
+    }
+
+    private ChangeListener<? super Boolean> sortByNameChangeListener() {
+        return (observableValue, oldValue, newValue) -> {
+            if (newValue) {
+                files = files.stream().sorted(Comparator.comparing(File::getName)).collect(Collectors.toCollection(LinkedList::new));
+                infoTextArea.clear();
+                infoTextArea.setText(files.stream().map(File::getName).collect(Collectors.joining("\n")));
+                sortByCreationDateDescCheckBox.setSelected(false);
+                sortByCreationDateAscCheckBox.setSelected(false);
+            }
+        };
     }
 }
